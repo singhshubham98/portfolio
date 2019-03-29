191400e5973177964abb8007d9789d012b6910b8
Shubham Singh <201652026@iiitvadodara.ac.in>
2019-03-29 21:01:44 +0530

    resume

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-// import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Navbar from "./components/navbar";
+import jQuery from "jquery";
+import Icons from "uikit/dist/js/uikit-icons";
+import UIkit from "uikit";
 
-ReactDOM.render(<Navbar />, document.getElementById("root"));
+import "uikit/dist/css/uikit.min.css";
+
+UIkit.use(Icons);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
